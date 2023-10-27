8bb4ffd34c8badf12db70c35da82717b041e2b3c
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-27 12:05:26 +0700

    intial commit

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using ModernDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+		public HomeViewModel HomeVM { get; set; }
+        public MoviesViewModel MoviesVM { get; set; }
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand MoviesViewCommand { get; set; }
+
+
+        private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+        public MainViewModel()
+        {
+			HomeVM = new HomeViewModel();
+			MoviesVM = new MoviesViewModel();	
+
+			CurrentView = HomeVM;
+
+			HomeViewCommand = new RelayCommand(o =>
+			{
+				CurrentView = HomeVM;
+			});
+			MoviesViewCommand = new RelayCommand(o =>
+			{
+				CurrentView = MoviesVM;
+			});
+        }
+
+    }
+}
