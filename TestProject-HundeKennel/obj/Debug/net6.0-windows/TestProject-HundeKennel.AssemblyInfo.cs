c7717669f249705a9af1990320cf3a5b72946065
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-30 09:20:19 +0700

    resolving ciflict adding changes

--- TestProject-HundeKennel/obj/Debug/net6.0-windows/TestProject-HundeKennel.AssemblyInfo.cs
+++ TestProject-HundeKennel/obj/Debug/net6.0-windows/TestProject-HundeKennel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject-HundeKennel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef0c52757d5288572b308f67ed201465792b1a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject-HundeKennel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject-HundeKennel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
