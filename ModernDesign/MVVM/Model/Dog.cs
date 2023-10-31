5018c39f896031ca5093a2643ecd15049bae79aa
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-31 11:56:03 +0700

    Dog class expanded and enum implemented Connectionstring changed to group database

--- ModernDesign/MVVM/Model/Dog.cs
+++ ModernDesign/MVVM/Model/Dog.cs
@@ -15,8 +15,39 @@ namespace ModernDesign.MVVM.Model
         public string MomPedigreeNumber { get; set; }
         public string Gender { get; set; }
         public bool IsDead { get; set; }
-        
+        // Expanded dog attributes
+        public string ChipNumber { get; set; }
+        public string DKKTitles { get; set; }
+        public string Titles { get; set; }
+        public bool BreedingStatus { get; set; }
+        public bool MentalDescription { get; set; }
+        public BinaryData Picture { get; set; }
+        public Illness Health { get; set; }
+        public ColorType Color { get; set; }
+        public BreedingApprovalType BreedingApproval { get; set; }
 
+        public Dog(string pedigreeNumber, string name, DateTime dOB, string dadPedigreeNumber, 
+            string momPedigreeNumber, string gender, bool isDead, string chipNumber, string dKKTitles, 
+            string titles, bool breedingStatus, bool mentalDescription, BinaryData picture,
+            Illness health, ColorType color, BreedingApprovalType breedingApproval)
+        {
+            PedigreeNumber = pedigreeNumber;
+            Name = name;
+            DOB = dOB;
+            DadPedigreeNumber = dadPedigreeNumber;
+            MomPedigreeNumber = momPedigreeNumber;
+            Gender = gender;
+            IsDead = isDead;
+            ChipNumber = chipNumber;
+            DKKTitles = dKKTitles;
+            Titles = titles;
+            BreedingStatus = breedingStatus;
+            MentalDescription = mentalDescription;
+            Picture = picture;
+            Health = health;
+            Color = color;
+            BreedingApproval = breedingApproval;
+        }
 
         public Dog(string pedigreeNumber,
                    string name,
---
+++ ModernDesign/MVVM/Model/Illness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVVM.Model
+{
+    public enum Illness
+    {
+        HD,
+        AD,
+        HZ,
+        SP,
+        None
+    }
+}
