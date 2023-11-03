de8a0561c09f627f69f66a3d69cea799a629768b
Adam Eleraky <adam_eleraky@hotmail.com>
2023-11-03 08:28:31 +0100

    DogOwner class created

--- ModernDesign/MVVM/Model/DogOwner.cs
+++ ModernDesign/MVVM/Model/DogOwner.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ModernDesign.MVVM.Model
 {
-    internal class DogOwner
+    public class DogOwner
     {
+        private string Name;
+        private string Address;
+        private string ZipCode;
+        private string City;
+        private string Phone;
+        private string Email;
+
+
+        public DogOwner(string name, string address, string zipCode, string city, string phone, string email)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipCode;
+            City = city;
+            Phone = phone;
+            Email = email;
+        }
     }
 }
